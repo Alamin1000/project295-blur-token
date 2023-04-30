37cc1f75ca8e780f6540ff6e1d4125aabd96274f
alamin <alaminhosen44000@gmail.com>
2023-04-30 20:22:04 +0600

    tooltip-added

--- js/main.js
+++ js/main.js
@@ -5,7 +5,11 @@
 $(document).ready(function () {
   // preloader
   $("#preloader").fadeOut(500);
-  
 
-
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  );
 });
